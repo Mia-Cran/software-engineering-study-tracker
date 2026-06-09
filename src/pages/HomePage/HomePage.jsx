03fc095bc7039b61bbccf753107ccc922f731f50
mia-cran <palmermaria1991@gmail.com>
2026-06-09 00:06:06 -0400

    created HomePage skeleton structure

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,14 @@
 function HomePage() {
   return (
-    <section className="app__intro">
-      <h1 className="app__title">Search Topics</h1>
-      <p className="app__description">
+    <section className="home">
+      <h1 className="home__title">Search Topics</h1>
+      <p className="home__description">
          Search for software engineering concepts and turn them into study cards.
       </p>
+      <form className="home__form">
+        <input className="home__input" type="text" placeholder='Try "JavaScript", "React", or "API"'/>
+        <button className="home__button" type="submit">Search</button> 
+      </form>
     </section>
   );
 }
