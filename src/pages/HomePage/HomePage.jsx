b103c1bcaceaf9202c6bfb017348af7657d86f6e
mia-cran <palmermaria1991@gmail.com>
2026-06-10 23:46:50 -0400

    Add search input state

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
+
 function HomePage() {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  function handleSearchSubmit(e) {
+    e.preventDefault();
+    console.log(searchQuery);
+  }
+
   return (
-    <section className="home">
-      <h1 className="home__title">Search Topics</h1>
+   <section className="home">
+   <h1 className="home__title">Search Topics</h1>
       <p className="home__description">
          Search for software engineering concepts and turn them into study cards.
       </p>
-      <form className="home__form">
-        <input className="home__input" type="text" placeholder='Try "JavaScript", "React", or "API"'/>
-        <button className="home__button" type="submit">Search</button> 
+      <form className="home__form" onSubmit={handleSearchSubmit}>
+        <input 
+          className="home__input" 
+          type="text" 
+          placeholder='Try "JavaScript", "React", or "API"'
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          />
+        <button className="home__button" type="submit">
+          Search</button> 
       </form>
     </section>
   );
