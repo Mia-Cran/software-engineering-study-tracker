1c92071e776e3025ed9bcbec435aa00d732fccbd
mia-cran <palmermaria1991@gmail.com>
2026-06-16 22:54:22 -0400

    Add welcome page image

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,8 +1,14 @@
 import "./WelcomePage.css";
+import welcomeImage from "../../assets/welcome-study.png";
 
 function WelcomePage() {
   return (
     <section className="app__intro">
+      <img
+        className="app__image"
+        src={welcomeImage}
+        alt="Open book with coding screen"
+      />
       <h1 className="app__title">Welcome to <br />
         Your Software Engineering Study Tracker</h1>
       <p className="app__description">
