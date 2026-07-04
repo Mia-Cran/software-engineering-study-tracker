0e993e2d15c0c0f3c700e7dc450a31708bd4e928
mia-cran <palmermaria1991@gmail.com>
2026-07-04 13:11:45 -0400

    Set up backend and connect MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
-const express = require("express");
+const mongoose = require("mongoose");
+const app = require("./app");
 
-const app = express();
 const PORT = 3001;
 
-app.get("/", (req, res) => {
-    res.send("Backend is running");
-});
+mongoose
+  .connect("mongodb://127.0.0.1:27017/study_tracker_db")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
