a0dbec7f970720d1bc88d439f3697b05f440efdd
mia-cran <palmermaria1991@gmail.com>
2026-07-10 15:10:32 -0400

    Completed saved topic feedback display and delete flow

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const topicRoutes = require("./routes/topics");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
